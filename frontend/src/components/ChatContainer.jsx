9858a5fb33aaa8bb87a059302f9484e98d138747
abhishekjadhav208 <abhishekjadhav5035@gmail.com>
2025-07-17 00:12:18 +0530

    Rename SignupPage.jsx to Signup.jsx and update references

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -1,10 +1,10 @@
-import { useChatStore } from "../pages/store/useChatStore";
+import { useChatStore } from "../store/useChatStore";
 import { useEffect, useRef } from "react";
 
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
 import MessageSkeleton from "./skeletons/MessageSkeleton";
-import { useAuthStore } from "../pages/store/useAuthStore";
+import { useAuthStore } from "../store/useAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
